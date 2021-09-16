c7ca768c10bf66e9f43a317f51f2de906e29f4e8
Nosa Edoimioya <nosed@UMICH.EDU>
2021-09-16 15:43:28 -0400

    Completed frontend layout and styling

--- pulse-reconstruction-app/src/homepage/index.js
+++ pulse-reconstruction-app/src/homepage/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './homepageStyles.css';
 import upload_icon from '../icons/upload_icon.png'
+import refresh_icon from '../icons/refresh_icon.png'
+import github_icon from '../icons/github_icon.png';
+import email_icon from '../icons/email_icon.png';
 
 function Homepage() {
     return (
@@ -28,7 +31,7 @@ function Homepage() {
                     Download your reconstructed pulses.
                 </p>
             </div>
-            <div className='step1Div'>
+            <div className='stepDiv'>
                 <div className='instructions'>
                     <h3>Step 1</h3>
                     <p>
@@ -41,8 +44,11 @@ function Homepage() {
                         <span style={{fontWeight:'bolder'}}>Check</span> to make sure the files look as expected. Once your data is uploaded, you can to proceed to Step 2.
                     </p>
                 </div>
-                <div style={{marginLeft:'64px',marginTop:'8px',display:'flex',flexDirection:'column'}}>
+                <div className='secondColumn' style={{marginTop:'8px'}}>
                     <div>
+                        {/* TODO: Handle file upload to Heroku
+                            TODO: Variable for button to show up after file upload
+                            TODO: Hide other steps until data is uploaded. */}
                         <p style={{marginBottom:'4px'}}>Upload data</p>
                         <label className='uploader'>
                             <input 
@@ -53,35 +59,125 @@ function Homepage() {
                             <p style={{color:'blue'}}>Click to upload data</p>
                         </label>
                     </div>
-                    <div style={{marginTop:'16px',display:'flex',flexDirection:'column'}}>
-                        <label>Number of pulses: </label>
-                        <input type='number' className='inputBox' required></input>
-                    </div>
-                    <div style={{marginTop:'8px',display:'flex',flexDirection:'column'}}>
-                        <label>Temporal Resolution: </label>
-                        <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-                            <input type='number' className='inputBox' style={{width: '272px'}} required></input>
-                            <p style={{marginLeft:'10px'}}>sec</p>
+                    <form>
+                        {/* TODO: states for input fields
+                        TODO: Passing states to the backend to be used for computation onFormSubmit
+                        TODO: Getting image data from the backend to populate data display
+                        TODO: Handle on submit behavior after button is clicked */}
+                        <div style={{marginTop:'16px',display:'flex',flexDirection:'column'}}>
+                            <label>Number of pulses: </label>
+                            <input type='number' className='inputBox' required></input>
                         </div>
-                    </div>
-                    <div style={{marginTop:'8px',display:'flex',flexDirection:'column'}}>
-                        <label>Frequency Resolution: </label>
-                        <div style={{margin:'0px',display:'flex',flexDirection:'row',alignItems:'center'}}>
-                            <input type='number' className='inputBox' style={{width: '272px'}} required></input>
-                            <p style={{marginLeft:'10px'}}>Hz</p>
+                        <div style={{marginTop:'8px',display:'flex',flexDirection:'column'}}>
+                            <label>Temporal Resolution: </label>
+                            <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                                <input type='number' className='inputBox' style={{width: '272px'}} required></input>
+                                <p style={{margin:'0px 0px 0px 10px'}}>sec</p>
+                            </div>
                         </div>
-                    </div>  
+                        <div style={{marginTop:'8px',display:'flex',flexDirection:'column'}}>
+                            <label>Frequency Resolution: </label>
+                            <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                                <input type='number' className='inputBox' style={{width: '272px'}} required></input>
+                                <p style={{margin:'0px 0px 0px 10px'}}>Hz</p>
+                            </div>
+                        </div>
+                        <button type='submit' className='greenButton'>Proceed to Step 2</button>
+                    </form> 
                 </div>
-                <div style={{marginLeft: '64px', marginTop:'8px'}}>
+                <div style={{marginLeft: '64px', marginTop:'8px', display:'flex',flexDirection:'column'}}>
                     <p style={{marginBottom:'4px'}}>Data display</p>
                     <div className='dataDisplay'>
-                        <h3 style={{marginLeft:'16px'}}>Your FROG Traces</h3>
+                        <h3 style={{marginBottom: '0px'}}>Your FROG Traces</h3>
+                        <div style={{marginTop: '32px'}}>
+                            <div className='examplePlot'></div>
+                        </div>
+                        <div style={{marginTop: '32px'}}>
+                            <div className='examplePlot'></div>
+                        </div>
                     </div>
-                    
                 </div>
             </div>
+            <div className='separator'></div>
+
+            <div className='stepDiv'>
+                <div className='instructions'>
+                    <h3>Step 2</h3>
+                    <p>
+                        The network is trained to predict a select number of pulse width ranges (given in femtoseconds).
+                    </p>
+                    <p>
+                        <span style={{fontWeight:'bolder'}}>Verify</span> the pulse width prediction is reasonable given your knowledge of the data source.
+                    </p>
+                </div>
+                <div className='secondColumn center' style={{marginTop:'56px'}}>
+                    <h3>Pulse Width Prediction:</h3>
+                    <div className='pulsePrediction'>
+                        {/* TODO: Change this to be a functional input from the backend */}
+                        55-65 fs
+                    </div>
+                </div>
+                {/* TODO: onClick behavior for button: conditionally render the next step */}
+                <button type='button' className='greenButton2'>Looks good!</button>
+            </div>
+            <div className='separator'></div>
+
+            <div className='stepDiv'>
+                <div className='instructions'>
+                    <h3>Step 3</h3>
+                    <p>
+                        <span style={{fontWeight:'bolder'}}>View</span> samples of reconstructed pulses for your FROG trace data by scrolling up and down.                    
+                    </p>
+                    <p>
+                        <span style={{fontWeight:'bolder'}}>Refresh</span> the box to view a new set of randomly selected samples.
+                    </p>
+                </div>
+                <div className='secondColumn frogDisplay'>
+                    <div className='frogDisplayTitle'>
+                        <h4 style={{marginLeft: '96px'}}>Your FROG Traces</h4>
+                        <h4 style={{marginLeft: '160px'}}>Reconstructed Pulses</h4>
+                        {/* TODO: change onClick behavior of refresh icon to fetch random samples from backend and populate the display wth images. */}
+                        <img src={refresh_icon} alt='Refresh' className='refresh' />
+                    </div>
+                    {/* one frog and pulse pair */}
+                    <div className='frogAndPulse'> 
+                        {/* frog */}
+                        <div className='examplePlot'></div> 
+                        {/* pulse */}
+                        <div className='examplePlot' style={{marginLeft: '64px'}}></div>
+                    </div>
+                    <div className='frogAndPulse'> 
+                        {/* frog */}
+                        <div className='examplePlot'></div> 
+                        {/* pulse */}
+                        <div className='examplePlot' style={{marginLeft: '64px'}}></div>
+                    </div>
+                </div>
+            </div>
+            <div className='separator'></div>
             
+            <div className='stepDiv'>
+                <div className='instructions'>
+                    <h3>Step 4</h3>
+                    <p>
+                        <span style={{fontWeight:'bolder'}}>Download</span> the reconstructed pulse data.                     
+                    </p>
+                    <p>
+                        The file contains indexed reconstructed pulses and their correponding FROG traces.
+                    </p>
+                </div>
+                {/* TODO: trigger download of files onClick */}
+                <button type='button' className='greenButton3'>Download reconstructed pulses</button>
+            </div>
             
+            <div className='footer'>
+                <h3>Yiming Gong - Department of Physics, University of Michigan, Ann Arbor, MI 48109 USA</h3>
+                <h3 style={{marginTop: '0px', marginBottom: '0px'}}>Nosa Edoimioya - Department of Mechanical Engineering, University of Michigan, Ann Arbor, MI 48109</h3>
+                <div className='contact'>
+                    <img src={github_icon} alt='Go to Github' style={{marginRight:'8px', cursor:'pointer'}}/>
+                    <img src={email_icon} alt='ymgong@umich.edu' style={{cursor:'pointer'}}/>
+                </div>
+            </div>
         </div>
     )
 }
